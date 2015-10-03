5beea444ded9d51381e99e212487bc577e8486b0
Saqib Shaikh <me@saqibshaikh.com>
2015-10-03 21:24:40 +0100

    Created Nuspec file for NuGet.

--- DotIni/Properties/AssemblyInfo.cs
+++ DotIni/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DotIni")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .Net library for working with .ini files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Saqib Shaikh")]
 [assembly: AssemblyProduct("DotIni")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015, Saqib Shaikh")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-# DotIni - A .Net library for working with .ini files
+DotIni - A .Net library for working with .ini files
 
 This library is a thin wrapper around the native Win32 functions for reading from, and writing to, .ini files.
 
